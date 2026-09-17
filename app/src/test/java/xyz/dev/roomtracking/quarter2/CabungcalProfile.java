7069d60f0ff6fbccea5a11df627ff9be33c6a239
My PC <micoxd81@gmail.com>
2026-09-17 15:20:57 +0800

    Added cabungcal profile test mini peta

---
+++ app/src/test/java/xyz/dev/roomtracking/quarter2/CabungcalProfile.java
@@ -0,0 +1,23 @@
+package xyz.dev.roomtracking.quarter2;
+
+import org.junit.Test;
+
+public class CabungcalProfile {
+    @Test
+    public void printMyprofile() {
+
+        // --- 1. THE INPUT (Storing your personal details in variables) ---
+        String myname = "Janssen";
+        String petName = "choco";
+        String favFood = "Sinigang";
+        int myage = 17;
+
+        // --- 2. THE OUTPUT (Printing to the console) ---
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myname + " and I am " + myage + " years old.");
+        System.out.println("I have wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
+}
+
+
